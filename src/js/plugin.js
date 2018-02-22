8525d210acebbc01bcf279246ba8a24b966f09e9
retrofuturo <pzabelina11@gmail.com>
2018-02-22 21:40:42 +0200

    finished

---
+++ src/js/plugin.js
@@ -0,0 +1,37 @@
+;(function(){
+    // sidebar mobile
+
+    var menuBtn = $('.menu-btn');
+    var body = $('body');
+    var mobileSidebarClass = '_mobileSidebar';
+    var mobileBreakPoint = 1280;
+    var openInMobileClass = '_openInmobile'
+
+    menuBtn.on('click', function(e){
+        var width = $(window).width();
+        body.toggleClass(mobileSidebarClass);
+        if (width < mobileBreakPoint) {
+            body.toggleClass(openInMobileClass);
+        }
+    });
+
+    $(window).on('resize load', function(e){
+        var width = $(this).width();
+
+        if(width < mobileBreakPoint && !body.hasClass(openInMobileClass)){
+            body.addClass(mobileSidebarClass);
+        } else if (width > mobileBreakPoint){
+            body.removeClass(mobileSidebarClass);
+            body.removeClass(openInMobileClass);
+        }
+    });
+
+    // loader hide
+    var loader = $('#loading');
+
+    $(window).on('load', function (e) {
+        loader.addClass('hide');
+    })
+
+})();
+
